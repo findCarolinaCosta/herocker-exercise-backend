c27e2265069ec5524f8ffa752ca69a1253a76562
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-18 15:02:11 -0300

    tentativa de resolver error do lint

--- src/sequelize/migrations/20211020194502-create-user.js
+++ src/sequelize/migrations/20211020194502-create-user.js
@@ -1,32 +1,36 @@
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      firstName: {
-        type: Sequelize.STRING,
-      },
-      lastName: {
-        type: Sequelize.STRING,
-      },
-      email: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+const Sequelize = require('sequelize');
+
+const attributes = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
   },
-  down: async (queryInterface, Sequelize) => {
+  firstName: {
+    type: Sequelize.STRING,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+};
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable('Users', attributes);
+  },
+  down: async (queryInterface) => {
     await queryInterface.dropTable('Users');
   },
 };
